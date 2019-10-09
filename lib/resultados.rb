7449154ca39351f8bbd92e2b7e0274e509a2ebf1
Gustavo Urbano <gustavocabralu@gmail.com>
2019-10-09 18:00:28 -0300

    Only 1

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -9,11 +9,15 @@ class Resultados
 
   def calcula_performance
     return 0 if valor_meta == 0
-    valor_realizado.to_f / valor_meta.to_f
+   
+      valor_realizado.to_f / valor_meta.to_f
+
   end
 
   def calcula_realizado
     return 0 if valor_realizado == 0
-    valor_meta.to_f * valor_performance.to_f
+  
+      valor_meta.to_f * valor_performance.to_f
+
   end
 end
