53c7b8f1b35458fce87045314123b3ec53100390
Gustavo Urbano <gustavocabralu@gmail.com>
2019-10-09 14:53:27 -0300

    Alguns Teste Feitos

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -1,19 +1,19 @@
 class Resultados
-  attr_accessor :valor_meta, :valor_realizado
+  attr_accessor :valor_meta, :valor_realizado, :valor_performance
 
   def initialize()
     @valor_meta = valor_meta
     @valor_realizado = valor_realizado
-    # @valor_performance = valor_performance
+    @valor_performance = valor_performance
   end
 
   def calcula_performance
-    return 0 if valor_meta.zero?
-    valor_realizado / valor_meta
+    return 0 if valor_meta == 0
+    valor_realizado.to_f / valor_meta.to_f
   end
 
   def calcula_realizado
-    return 0 if valor_realizado.zero?
-    valor_meta * valor_peformance
+    return 0 if valor_realizado == 0
+    valor_meta.to_f * valor_performance.to_f
   end
 end
--- spec/lib/calculos_spec.rb
+++ spec/lib/calculos_spec.rb
@@ -6,21 +6,25 @@ RSpec.describe Calculos, type: :lib do
     it "com atributos validos" do
       valor_performance = Calculos.performance(100.5, 150.5)
 
-      expect(valor_performance).to eq(1.1682242990654206)
+      expect(valor_performance).to eq(1.4975124378109452)
     end
 
     it "com valor realizado valido e meta zero" do
       valor_performance = Calculos.performance(0.0, 150.5)
 
-      expect(valor_performance).to eq(0)
+      expect(valor_performance.infinite?).to eq(1)
     end
 
     it "com valor realizado valido e meta null" do
-      skip("escreva testes para esses casos")
+      valor_performance = Calculos.performance(nil, 150.5)
+
+      expect(valor_performance.infinite?).to eq(1)
     end
 
     it "com valor meta valido e realizado zero" do
-      skip("escreva testes para esses casos")
+      valor_performance = Calculos.performance(150.5, 0)
+
+      expect(valor_performance).to eq(0)
     end
   end
 end
