3638a5e8fabfaa299d63195b4a008f980dd7757d
Caroline Rezende <caroline6254@hotmail.com>
2022-01-19 10:26:50 -0300

    fix regex self.data method/ write data testesgst

--- spec/lib/validadores_spec.rb
+++ spec/lib/validadores_spec.rb
@@ -5,31 +5,41 @@ RSpec.describe Validadores, type: :lib do
   describe "Validadores" do
     context "data" do
       it "validador data (YYYY-MM-DD)" do
-        data = Validadores.data('2019-31-12')
+        data = Validadores.data('2019-12-31')
 
         expect(data).to eq(true)
       end
 
       it "validador data (YYYY-MM)" do
-        skip("escreva testes para esses casos")
+        data = Validadores.data('2019-12')
+
+        expect(data).to eq(true)
       end
 
       it "validador para data (DD/MM/YYYY)" do
-        skip("escreva testes para esses casos")
+        data = Validadores.data('31/12/2019')
+
+        expect(data).to eq(true)
       end
     end
 
     context "número" do
       it "validador número inteiro" do
-        skip("escreva testes para esses casos")
+        numero = Validadores.valor('35')
+
+        expect(valor).to eq(true)
       end
 
       it "validador número decimal" do
-        skip("escreva testes para esses casos")
+        numero = Validadores.valor('0.2')
+
+        expect(valor).to eq(true)
       end
 
       it "validador número percentual" do
-        skip("escreva testes para esses casos")
+        numero = Validadores.valor('20%')
+
+        expect(valor).to eq(true)
       end
     end
 
