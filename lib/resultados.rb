13a0de88fae9c1f81b88a75e7f4fdf8fdeb8d8f2
Caroline Rezende <caroline6254@hotmail.com>
2022-01-19 11:28:43 -0300

    add conditionals to calcula_performance / initialize @valor_performance

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -1,19 +1,26 @@
 class Resultados
-  attr_accessor :valor_meta, :valor_realizado
+  attr_accessor :valor_meta, :valor_realizado, :valor_performance
 
   def initialize()
     @valor_meta = valor_meta
     @valor_realizado = valor_realizado
-    # @valor_performance = valor_performance
+    @valor_performance = valor_performance
   end
 
   def calcula_performance
-    return 0 if valor_meta.zero?
-    valor_realizado / valor_meta
+    if valor_meta.nil? then
+      return 0
+    elsif valor_meta == 0
+      return nil
+    elsif valor_realizado == 0
+      return 1
+    else
+      valor_realizado.to_f / valor_meta.to_f
+    end
   end
 
   def calcula_realizado
-    return 0 if valor_realizado.zero?
-    valor_meta * valor_peformance
+
+    valor_meta * valor_performance
   end
 end
