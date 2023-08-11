c3eec9db50d7a91a8a7bc3b525cef494c58c2fe2
Guillaume Zeldine <g.zeldine@hotmail.fr>
2023-08-10 23:19:44 -0300

    Permitting Valor Performance to Resultados Controllers, fixing calculos return

--- app/controllers/calculos_controller.rb
+++ app/controllers/calculos_controller.rb
@@ -3,7 +3,6 @@ require 'calculos'
 class CalculosController < ApplicationController
   def performance
     valor_performance = Calculos.performance(params[:valor_meta], params[:valor_realizado])
-
-    render json: { valor_performance: valor_performance }
+    render json: { valor_performance: }
   end
 end
