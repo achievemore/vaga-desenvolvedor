54fa9e16d2b7abfdbce11ef453cc7cf4d88298df
Guillaume Zeldine <g.zeldine@hotmail.fr>
2023-08-10 23:20:55 -0300

    Testing Calculos and adding some desired returns

--- lib/calculos.rb
+++ lib/calculos.rb
@@ -1,5 +1,11 @@
 class Calculos
   def self.performance(valor_meta, valor_realizado)
+    return 0 if valor_meta.nil? || valor_realizado.nil?
+
+    valor_realizado = valor_realizado.to_f
+    valor_meta = valor_meta.to_f
+    return 0 if valor_meta.zero? || valor_realizado.zero?
+
     (valor_realizado / valor_meta)
   end
 end
