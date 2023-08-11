9637fcfad323e008386ec9825febf61a795834fe
Guillaume Zeldine <g.zeldine@hotmail.fr>
2023-08-10 23:21:46 -0300

    Enabling attr_accessor valor_performance, adding desired returns, also testing resultados

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -1,19 +1,22 @@
 class Resultados
-  attr_accessor :valor_meta, :valor_realizado
+  attr_accessor :valor_meta, :valor_realizado, :valor_performance
 
   def initialize()
     @valor_meta = valor_meta
     @valor_realizado = valor_realizado
-    # @valor_performance = valor_performance
+    @valor_performance = valor_performance
   end
 
   def calcula_performance
-    return 0 if valor_meta.zero?
-    valor_realizado / valor_meta
+    return 0 if valor_meta.nil?
+    return nil if valor_meta.zero?
+    return 1 if valor_realizado.zero?
+
+    @valor_performance = valor_realizado / valor_meta
   end
 
   def calcula_realizado
-    return 0 if valor_realizado.zero?
-    valor_meta * valor_peformance
+    return nil if valor_performance.nil? || valor_meta.nil?
+    @valor_realizado = valor_meta * valor_performance
   end
 end
--- spec/lib/resultados_spec.rb
+++ spec/lib/resultados_spec.rb
@@ -37,14 +37,44 @@ RSpec.describe Resultados, type: :lib do
   end
 
   describe "Calcular realizado" do
-    it "com valor realizado valido" do
+    it "com atributos valido" do
       resultado = Resultados.new
-      resultado.valor_performance = 0
-      resultado.valor_neta = 0
+      resultado.valor_performance = 10
+      resultado.valor_meta = 20
 
-      expect(resultado.calcula_realizado).to eq(1)
+      expect(resultado.calcula_realizado).to eq(200)
     end
 
-    # @TODO CRIE MAIS TESTES
+    it "com valor meta valido e performance zero" do
+      resultado = Resultados.new
+      resultado.valor_meta = 0
+      resultado.valor_performance = 20
+
+      expect(resultado.calcula_realizado).to eq(0)
+    end
+
+    it "com valor meta null e performance valido" do
+      resultado = Resultados.new
+      resultado.valor_meta = nil
+      resultado.valor_performance = 20
+
+      expect(resultado.calcula_realizado).to eq(nil)
+    end
+
+    it "com valor meta valido e performance null" do
+      resultado = Resultados.new
+      resultado.valor_meta = 20
+      resultado.valor_performance = nil
+
+      expect(resultado.calcula_realizado).to eq(nil)
+    end
+
+    it "com valor meta valido e performance zero" do
+      resultado = Resultados.new
+      resultado.valor_meta = 10
+      resultado.valor_performance = 0
+
+      expect(resultado.calcula_realizado).to eq(0)
+    end
   end
 end
