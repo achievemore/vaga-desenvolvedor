c919d64be051c2f41ac7c95fc4b5d29c0b37f89c
Leandro Machado Pereira <llpereiras@gmail.com>
2024-01-17 23:52:20 -0300

    feat: refactor of importa arquivos and fix specs

--- spec/controllers/resultados_controller_spec.rb
+++ spec/controllers/resultados_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ResultadosController, type: :controller do
-  let(:arquivo_valido) { { file: fixture_file_upload('metas_validas.csv', 'text/csv') } }
-
   let(:cliente) {
     create(:cliente)
   }
@@ -11,20 +9,22 @@ RSpec.describe ResultadosController, type: :controller do
     { cliente_id: cliente.id, periodo: Date.today, valor_meta: 10, valor_realizado: 12 }
   }
 
+  let(:resultado) { Resultado.create! valid_attributes }
+
   let(:valid_session) { {} }
 
   describe "GET #index" do
     it "returns a success response" do
-      resultado = Resultado.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index, params: {}
+
       expect(response).to be_successful
     end
   end
 
   describe "GET #show" do
     it "returns a success response" do
-      resultado = Resultado.create! valid_attributes
-      get :show, params: {id: resultado.to_param}, session: valid_session
+      get :show, params: {id: resultado.to_param}
+
       expect(response).to be_successful
     end
   end
@@ -42,22 +42,23 @@ RSpec.describe ResultadosController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { valor_meta: 10.5, valor_realizado: 12.7 }
       }
 
-      it "updates the requested resultado" do
-        resultado = Resultado.create! valid_attributes
+      it "updates the requested resultado", :aggregate_failures do
         put :update, params: {id: resultado.to_param, resultado: new_attributes}, session: valid_session
+
         resultado.reload
-        skip("Add assertions for updated state")
+
+        expect(resultado.valor_meta).to eq(10.5)
+        expect(resultado.valor_realizado).to eq(12.7)
       end
 
       it "renders a JSON response with the resultado" do
-        resultado = Resultado.create! valid_attributes
+        put :update, params: {id: resultado.to_param, resultado: valid_attributes}
 
-        put :update, params: {id: resultado.to_param, resultado: valid_attributes}, session: valid_session
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json')
+        expect(response.content_type).to include('application/json')
       end
     end
   end
@@ -65,15 +66,16 @@ RSpec.describe ResultadosController, type: :controller do
   describe "DELETE #destroy" do
     it "destroys the requested resultado" do
       resultado = Resultado.create! valid_attributes
+
       expect {
-        delete :destroy, params: {id: resultado.to_param}, session: valid_session
+        delete :destroy, params: {id: resultado.to_param}
       }.to change(Resultado, :count).by(-1)
     end
   end
 
   describe "Teste final!" do
     it "qual a resposta para a vida o universo e tudo mais?" do
-      resposta = Base64.encode64("ESCREVA AQUI A RESPOSTA")
+      resposta = Base64.encode64("42")
       expect("NDI=\n").to eq(resposta)
     end
   end
